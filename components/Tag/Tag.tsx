d8a68809217936b7a3fbc8954edc49bbe96f1a59
IxxyDev <denisenkoforwork@gmail.com>
2021-09-08 13:50:00 +0300

    feature: add tag

---
+++ components/Tag/Tag.tsx
@@ -0,0 +1,23 @@
+import { TagProps } from "./Tag.props";
+import styles from "./P.module.css";
+import cn from 'classnames';
+
+export const Tag = ({ size = 'sm', children, color = 'ghost', href, className, ...props }: TagProps): JSX.Element => {
+  return (
+    <p
+      className={cn(styles.tag, className, {
+        [styles.sm]: size == "sm",
+        [styles.m]: size == "m",
+        [styles.ghost]: color == "ghost",
+        [styles.gray]: color == "gray",
+        [styles.red]: color == "red",
+        [styles.green]: color == "green",
+        [styles.primary]: color == "primary",
+      })}
+      {...props}
+    >{
+      href ? <a href={href}>{children}</a> : <>{children}</>
+    }
+    </p>
+  );
+};
