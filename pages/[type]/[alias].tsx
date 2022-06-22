8b0f849dd8246f5c1ad891402feebe44f8850136
IxxyDev <denisenkoforwork@gmail.com>
2022-06-22 19:04:50 +0300

    feat: add meta tags

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
-import { withLayout } from '../../layout/Layout';
+import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from 'next';
+import {withLayout} from '../../layout/Layout';
 import axios from 'axios';
-import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface';
-import { MenuItem } from '../../interfaces/menu.interface';
-import { ProductModel } from '../../interfaces/product.interface';
-import { firstLevelMenu } from '../../helpers/helpers';
-import { TopPageComponent } from '../../page-components';
+import {TopLevelCategory, TopPageModel} from '../../interfaces/page.interface';
+import {MenuItem} from '../../interfaces/menu.interface';
+import {ProductModel} from '../../interfaces/product.interface';
+import {firstLevelMenu} from '../../helpers/helpers';
+import {TopPageComponent} from '../../page-components';
 import {API} from "../../helpers/api";
+import Head from 'next/head';
 
-function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
-  return <TopPageComponent
-    firstCategory={firstCategory}
-    page={page}
-    products={products}
-  />;
+function TopPage({firstCategory, page, products}: TopPageProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>{page.metaTitle}</title>
+        <meta name='description' content={page.metaDescription} />
+        <meta property='og:title' content={page.metaTitle} />
+        <meta property='og:description' content={page.metaDescription} />
+        <meta property='og:type' content='article' />
+      </Head>
+      <TopPageComponent
+        firstCategory={firstCategory}
+        page={page}
+        products={products}
+      />
+    </>
+
+  );
 }
 
 export default withLayout(TopPage);
@@ -22,10 +35,10 @@ export default withLayout(TopPage);
 export const getStaticPaths: GetStaticPaths = async () => {
   let paths: string[] = [];
   for (const m of firstLevelMenu) {
-    const { data: menu } = await axios.post<MenuItem[]>(
+    const {data: menu} = await axios.post<MenuItem[]>(
       API.topPage.find,
       {
-       firstCategory: m.id,
+        firstCategory: m.id,
       }
     );
     paths = paths.concat(menu.flatMap(s => s.pages.map(p => `/${m.route}/${p.alias}`)));
@@ -36,7 +49,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps<TopPageProps> = async ({ params }: GetStaticPropsContext) => {
+export const getStaticProps: GetStaticProps<TopPageProps> = async ({params}: GetStaticPropsContext) => {
   if (!params) {
     return {
       notFound: true
@@ -49,7 +62,7 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({ params }: G
     };
   }
   try {
-    const { data: menu } = await axios.post<MenuItem[]>(
+    const {data: menu} = await axios.post<MenuItem[]>(
       API.topPage.find,
       {
         firstCategory: firstCategoryItem.id,
@@ -60,23 +73,23 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({ params }: G
         notFound: true
       };
     }
-    const { data: page } = await axios.get<TopPageModel>(
-     API.topPage.byAlias + params.alias
+    const {data: page} = await axios.get<TopPageModel>(
+      API.topPage.byAlias + params.alias
     );
-    const { data: products } = await axios.post<ProductModel[]>(
-     API.product.find, {
-       category: page.category,
-       limit: 10
-     }
-   );
-   return {
-    props: {
-      menu,
-      firstCategory: firstCategoryItem.id,
-      page,
-      products,
-    },
-   };
+    const {data: products} = await axios.post<ProductModel[]>(
+      API.product.find, {
+        category: page.category,
+        limit: 10
+      }
+    );
+    return {
+      props: {
+        menu,
+        firstCategory: firstCategoryItem.id,
+        page,
+        products,
+      },
+    };
   } catch {
     return {
       notFound: true
